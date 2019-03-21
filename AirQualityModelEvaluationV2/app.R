f5b3c5f931a64975b60064d073d010eb8f31cc65
JimShady <2901470+JimShady@users.noreply.github.com>
2019-03-21 21:36:00 +0000

    made alot of changes to dashboard

--- AirQualityModelEvaluationV2/app.R
+++ AirQualityModelEvaluationV2/app.R
@@ -1,5 +1,8 @@
 library(shinydashboard)
-library(dplyr)
+library(shiny)
+library(shinyWidgets)
+library(tidyverse)
+library(openair)
 
 
 ui <- dashboardPage(skin='red', # Start dashboardpage
@@ -105,13 +108,33 @@ ui <- dashboardPage(skin='red', # Start dashboardpage
               h2("Model comparisons"),
               
               fluidRow( # Start of a row
+                column(width=3,
+                box(width = NULL, title = 'Model Controls 1', status = 'primary', solidHeader = T,
+                    uiOutput('file1pollutantselector'),
+                    uiOutput('file1sitetypeselector'),
+                    uiOutput('file1sitecodeselector')),
+                box(width = NULL, title = "Model 1 Stats", tableOutput('table1')),
+                box(width = NULL, title = 'Model Controls 2', status = 'primary', solidHeader = T,
+                    uiOutput('file2pollutantselector'),
+                    uiOutput('file2sitetypeselector'),
+                    uiOutput('file2sitecodeselector')),
+                box(width = NULL, title = 'Model 2 Stats', tableOutput('table2'))
+                ),
                 column(width=6,
-                box(width = NULL, title = 'Model Controls 1', status = 'primary', solidHeader = T),
-                box(width = NULL, title = 'Model Controls 2', status = 'primary', solidHeader = T)),
-                column(width=6,
-                box(width = NULL, title = "Model 1 Graph"),
-                box(width = NULL, offset = 6, title = 'Model 2 Graph')
-                )
+                box(width = NULL, title = "Model 1 Graph", 
+                    plotOutput('graph1',
+                    click = "plot_click1",
+                    dblclick = "plot_dblclick1",
+                    hover = "plot_hover1",
+                    brush = "plot_brush1")),
+                box(width = NULL, title = "Model 2 Graph",
+                    plotOutput('graph2',
+                    click = "plot_click2",
+                    dblclick = "plot_dblclick2",
+                    hover = "plot_hover2",
+                    brush = "plot_brush2"))
+                ),
+                column(width = 3)
               ) # End of a row
               
       ) # end third tab content
@@ -123,68 +146,58 @@ ui <- dashboardPage(skin='red', # Start dashboardpage
 
 server <- function(input, output) {
   
-  output$contents1 <- renderTable({ # Start of uploading file 1
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
-    
+  dataframe1 <- reactive({
     req(input$file1)
-    
     # when reading semicolon separated files,
     # having a comma separator causes `read.csv` to error
     tryCatch(
       {
-        df1 <- read.csv(input$file1$datapath,
-                       header = input$header1,
-                       sep = input$sep1,
-                       quote = input$quote1)
+        read.csv(input$file1$datapath,
+                        header = input$header1,
+                        sep = input$sep1,
+                        quote = input$quote1,
+                        stringsAsFactors = F)
+        
       },
       error = function(e) {
         # return a safeError if a parsing error occurs
         stop(safeError(e))
       }
     )
+  })
+  
+  output$contents1 <- renderTable({ dataframe1() }) # End of uploading file 1
 
-    df1
-    
-  }) # End of uploading file 1
-
-  output$contents2 <- renderTable({ # Start of uploading file 2
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
-    
+  
+  dataframe2 <- reactive({
     req(input$file2)
-    
     # when reading semicolon separated files,
     # having a comma separator causes `read.csv` to error
     tryCatch(
       {
-        df2 <- read.csv(input$file2$datapath,
-                        header = input$header2,
-                        sep = input$sep2,
-                        quote = input$quote2)
+        read.csv(input$file2$datapath,
+                 header = input$header2,
+                 sep = input$sep2,
+                 quote = input$quote2,
+                 stringsAsFactors = F)
       },
       error = function(e) {
         # return a safeError if a parsing error occurs
         stop(safeError(e))
       }
     )
-    
-    df2
-    
-  }) # End of uploading file 2
+  })
+  
+  output$contents2 <- renderTable({ dataframe2() }) # End of uploading file 1
     
   ## Sort out the files for input to the graphs
   
-    model1modelled <- reactive({ input$file1 %>%
+    model1modelled <- reactive({ dataframe1() %>%
       select(-SiteID, -month, -Year, -Type, -SiteName,- MeasuredNOX, -MeasuredNO2, -MeasuredPM10, -MeasuredPM25, -MeasuredO3) %>% 
       rename_all(tolower) %>%
       gather(key = 'pollutant', value = 'modelled', 3:7) %>%
       mutate(sitetype = tolower(sitetype)) %>%
-      mutate(pollutant = gsub('model', '', pollutant),
+      mutate(pollutant = gsub('model', '', as.character(pollutant)),
             sitetype  = case_when(grepl('heathrow'          , sitetype) ~ 'Heathrow',
                                   grepl('traffic'           , sitetype) ~ 'Kerbside',
                                   grepl('kerbside'          , sitetype) ~ 'Kerbside',
@@ -194,12 +207,12 @@ server <- function(input, output) {
                                   grepl('urban background'  , sitetype) ~ 'Urban background'))
     })
     
-    model1measured <- reactive({input$file1 %>% 
+    model1measured <- reactive({ dataframe2() %>% 
       select(-SiteID, -month, -Year, -Type, -SiteName,- ModelNOx, -ModelNO2, -ModelPM10, -ModelPM25, -ModelO3) %>% 
       rename_all(tolower) %>%
       gather(key = 'pollutant', value = 'measured', 3:7) %>%
       mutate(sitetype = tolower(sitetype)) %>%
-      mutate(pollutant = gsub('measured', '', pollutant),
+      mutate(pollutant = gsub('measured', '', as.character(pollutant)),
              sitetype  = case_when(grepl('heathrow'          , sitetype) ~ 'Heathrow',
                                    grepl('traffic'           , sitetype) ~ 'Kerbside',
                                    grepl('kerbside'          , sitetype) ~ 'Kerbside',
@@ -209,48 +222,178 @@ server <- function(input, output) {
                                    grepl('urban background'  , sitetype) ~ 'Urban background'))
     })
     
-    data1     <- reactive({left_join(model1modelled, model1measured) %>% filter(modelled != 0 & measured != 0)})
+    data1     <- reactive({left_join(model1modelled(), model1measured()) %>% filter(modelled != 0 & measured != 0)})
     
-    table_data <- reactive({input$file1 %>% rename_all(tolower) %>% select(-siteid, -month, -year, -type, -sitename)})
+    table_data1 <- reactive({dataframe1() %>% rename_all(tolower) %>% select(-siteid, -month, -year, -type, -sitename)})
+    
+    output$file1pollutantselector <- renderUI({
+      selectInput("pollutant1", "pollutant:",  order(unique(as.character(data1()$pollutant))))
+    })
+    
+    output$file1sitetypeselector <- renderUI({
+      pickerInput(input="sitetype1",
+                  label="sitetype:",
+                  choices=unique(data1()$sitetype),
+                  options = list(`actions-box` = TRUE),
+                  multiple = T,
+                  selected = unique(data1()$sitetype))
+    })
+    
+    output$file1sitecodeselector <- renderUI({
+      pickerInput(input="sitecode1",
+                  label="sitecode:",
+                  choices=unique(data1()$sitecode),
+                  options = list(`actions-box` = TRUE),
+                  multiple = T,
+                  selected = unique(data1()$sitecode))
+    })
+    
+    ## Make the stats summary table
+    output$table1 <-renderTable({
+      #filter(
+        data1() %>% #, 
+             #pollutant == input$pollutant, sitetype %in% input$sitetype & sitecode %in% input$sitecode) %>%
+        modStats(mod = 'modelled',
+                 obs = 'measured') %>%
+        as_tibble() %>% select(-default)
+      
+    }, align='l')
+    
+    output$graph1 <- renderPlot(
+      {
+        ggplot(filter(data1(), pollutant == output$file1pollutantselector$pollutant1), aes(x = measured, y = modelled, label = sitecode, colour = sitetype)) + 
+          geom_point() +
+          coord_fixed() +
+          xlab('Measured') +
+          ylab('Modelled') +
+          #xlim(-5,
+          #     max(c(filter(data, pollutant == input$pollutant, sitetype %in% input$sitetype & sitecode %in% input$sitecode)$modelled,
+          #           filter(data, pollutant == input$pollutant, sitetype %in% input$sitetype)$measured))) +
+          #ylim(-5,
+          #     max(c(filter(data, pollutant == input$pollutant, sitetype %in% input$sitetype & sitecode %in% input$sitecode)$modelled,
+          #           filter(data, pollutant == input$pollutant, sitetype %in% input$sitetype)$measured))) +
+          theme(legend.title     = element_blank(),
+                panel.border     = element_blank(),
+                axis.title       = element_text(size=14, colour = 'black'),
+                axis.text        = element_text(size=14, colour = 'black'),
+                legend.text      = element_text(size=12, colour = 'black'),
+                legend.position  = 'right',
+                legend.spacing.x = unit(0.7, 'cm'),
+                plot.margin      = unit(c(0,0,0,0), "cm"),
+                panel.spacing     = unit(c(-1,-1,-1,-1), "cm"),
+                axis.line        = element_line(colour='black'),
+                panel.background = element_rect(fill = 'black'),
+                panel.grid.major = element_line(colour = 'grey'),
+                panel.grid.minor = element_blank()) +
+          geom_abline(intercept = 0, slope = 1, colour='white') + 
+          geom_abline(intercept = 0, slope = 2, linetype="dashed", colour='white') +
+          geom_abline(intercept = 0, slope = 0.5, linetype="dashed", colour='white') +
+          guides(guide_legend(ncol = 4))
+        
+      })
+    
+    ### Now the same for file 2
+    
+    model2modelled <- reactive({ dataframe2() %>%
+        select(-SiteID, -month, -Year, -Type, -SiteName,- MeasuredNOX, -MeasuredNO2, -MeasuredPM10, -MeasuredPM25, -MeasuredO3) %>% 
+        rename_all(tolower) %>%
+        gather(key = 'pollutant', value = 'modelled', 3:7) %>%
+        mutate(sitetype = tolower(sitetype)) %>%
+        mutate(pollutant = gsub('model', '', pollutant),
+               sitetype  = case_when(grepl('heathrow'          , sitetype) ~ 'Heathrow',
+                                     grepl('traffic'           , sitetype) ~ 'Kerbside',
+                                     grepl('kerbside'          , sitetype) ~ 'Kerbside',
+                                     grepl('industrial'        , sitetype) ~ 'Industrial',
+                                     grepl('roadside'          , sitetype) ~ 'Roadside',
+                                     grepl('suburban'          , sitetype) ~ 'Suburban background',
+                                     grepl('urban background'  , sitetype) ~ 'Urban background'))
+    })
+    
+    model2measured <- reactive({ dataframe2() %>% 
+        select(-SiteID, -month, -Year, -Type, -SiteName,- ModelNOx, -ModelNO2, -ModelPM10, -ModelPM25, -ModelO3) %>% 
+        rename_all(tolower) %>%
+        gather(key = 'pollutant', value = 'measured', 3:7) %>%
+        mutate(sitetype = tolower(sitetype)) %>%
+        mutate(pollutant = gsub('measured', '', pollutant),
+               sitetype  = case_when(grepl('heathrow'          , sitetype) ~ 'Heathrow',
+                                     grepl('traffic'           , sitetype) ~ 'Kerbside',
+                                     grepl('kerbside'          , sitetype) ~ 'Kerbside',
+                                     grepl('industrial'        , sitetype) ~ 'Industrial',
+                                     grepl('roadside'          , sitetype) ~ 'Roadside',
+                                     grepl('suburban'          , sitetype) ~ 'Suburban background',
+                                     grepl('urban background'  , sitetype) ~ 'Urban background'))
+    })
+    
+    data2     <- reactive({left_join(model2modelled(), model2measured()) %>% filter(modelled != 0 & measured != 0)})
+    
+    table_data2 <- reactive({dataframe2() %>% rename_all(tolower) %>% select(-siteid, -month, -year, -type, -sitename)})
   
-  output$graph2 <- reactive({
+    output$file2pollutantselector <- renderUI({
+      selectInput("pollutant2", "pollutant:",  order(unique(as.character(data2()$pollutant))))
+    })
     
-    model2modelled <- input$file2 %>%
-      select(-SiteID, -month, -Year, -Type, -SiteName,- MeasuredNOX, -MeasuredNO2, -MeasuredPM10, -MeasuredPM25, -MeasuredO3) %>% 
-      rename_all(tolower) %>%
-      gather(key = 'pollutant', value = 'modelled', 3:7) %>%
-      mutate(sitetype = tolower(sitetype)) %>%
-      mutate(pollutant = gsub('model', '', pollutant),
-             sitetype  = case_when(grepl('heathrow'          , sitetype) ~ 'Heathrow',
-                                   grepl('traffic'           , sitetype) ~ 'Kerbside',
-                                   grepl('kerbside'          , sitetype) ~ 'Kerbside',
-                                   grepl('industrial'        , sitetype) ~ 'Industrial',
-                                   grepl('roadside'          , sitetype) ~ 'Roadside',
-                                   grepl('suburban'          , sitetype) ~ 'Suburban background',
-                                   grepl('urban background'  , sitetype) ~ 'Urban background'))
+    output$file2sitetypeselector <- renderUI({
+      pickerInput(input="sitetype2",
+                  label="sitetype:",
+                  choices=unique(data2()$sitetype),
+                  options = list(`actions-box` = TRUE),
+                  multiple = T,
+                  selected = unique(data2()$sitetype))
+    })
     
-    model2measured <- input$file2 %>% 
-      select(-SiteID, -month, -Year, -Type, -SiteName,- ModelNOx, -ModelNO2, -ModelPM10, -ModelPM25, -ModelO3) %>% 
-      rename_all(tolower) %>%
-      gather(key = 'pollutant', value = 'measured', 3:7) %>%
-      mutate(sitetype = tolower(sitetype)) %>%
-      mutate(pollutant = gsub('measured', '', pollutant),
-             sitetype  = case_when(grepl('heathrow'          , sitetype) ~ 'Heathrow',
-                                   grepl('traffic'           , sitetype) ~ 'Kerbside',
-                                   grepl('kerbside'          , sitetype) ~ 'Kerbside',
-                                   grepl('industrial'        , sitetype) ~ 'Industrial',
-                                   grepl('roadside'          , sitetype) ~ 'Roadside',
-                                   grepl('suburban'          , sitetype) ~ 'Suburban background',
-                                   grepl('urban background'  , sitetype) ~ 'Urban background'))
+    output$file2sitecodeselector <- renderUI({
+      pickerInput(input="sitecode2",
+                  label="sitecode:",
+                  choices=unique(data2()$sitecode),
+                  options = list(`actions-box` = TRUE),
+                  multiple = T,
+                  selected = unique(data2()$sitecode))
+    })
     
+    ## Make the stats summary table
+    output$table2 <-renderTable({
+      #filter(
+      data2() %>% #, 
+        #pollutant == input$pollutant, sitetype %in% input$sitetype & sitecode %in% input$sitecode) %>%
+        modStats(mod = 'modelled',
+                 obs = 'measured') %>%
+        as_tibble() %>% select(-default)
+      
+    }, align='l')
     
-    data2     <- left_join(model2modelled, model2measured) %>% filter(modelled != 0 & measured != 0)
-    
-    rm(model2modelled, model2measured)
-    
-    table_data <- input$file2 %>% rename_all(tolower) %>% select(-siteid, -month, -year, -type, -sitename)
-    
-  })
+    output$graph2 <- renderPlot(
+      {
+        ggplot(data2(), aes(x = measured, y = modelled, label = sitecode, colour = sitetype)) + 
+          geom_point() +
+          coord_fixed() +
+          xlab('Measured') +
+          ylab('Modelled') +
+          #xlim(-5,
+          #     max(c(filter(data, pollutant == input$pollutant, sitetype %in% input$sitetype & sitecode %in% input$sitecode)$modelled,
+          #           filter(data, pollutant == input$pollutant, sitetype %in% input$sitetype)$measured))) +
+          #ylim(-5,
+          #     max(c(filter(data, pollutant == input$pollutant, sitetype %in% input$sitetype & sitecode %in% input$sitecode)$modelled,
+          #           filter(data, pollutant == input$pollutant, sitetype %in% input$sitetype)$measured))) +
+          theme(legend.title     = element_blank(),
+                panel.border     = element_blank(),
+                axis.title       = element_text(size=14, colour = 'black'),
+                axis.text        = element_text(size=14, colour = 'black'),
+                legend.text      = element_text(size=12, colour = 'black'),
+                legend.position  = 'right',
+                legend.spacing.x = unit(0.7, 'cm'),
+                plot.margin      = unit(c(0,0,0,0), "cm"),
+                panel.spacing     = unit(c(-1,-1,-1,-1), "cm"),
+                axis.line        = element_line(colour='black'),
+                panel.background = element_rect(fill = 'black'),
+                panel.grid.major = element_line(colour = 'grey'),
+                panel.grid.minor = element_blank()) +
+          geom_abline(intercept = 0, slope = 1, colour='white') + 
+          geom_abline(intercept = 0, slope = 2, linetype="dashed", colour='white') +
+          geom_abline(intercept = 0, slope = 0.5, linetype="dashed", colour='white') +
+          guides(guide_legend(ncol = 4))
+        
+      })
+  
   
 }
 
